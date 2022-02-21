011cc6ffe6a7ed2c2fc5a086fc88c6efb05bc6fb
jaswanthvaithiswaran <jaswanthvaithiswaran@gmail.com>
2022-02-21 23:39:14 +0530

    nav test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import Menu from "./Home/Menu";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Menu/>
+    <h1 class="text-blue-900 font-bold italic">
+      Hello world!
+    </h1>
+    </>
+  )
 }
-
-export default App;
---
+++ frontend/src/Home/Menu.js
@@ -0,0 +1,22 @@
+import {Link} from "react-router-dom";
+
+export default function Menu(){
+    return(
+        
+        <ul class="flex">
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+        </li>
+        <li class="mr-6">
+          <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
+        </li>
+      </ul>
+       
+    )
+}
